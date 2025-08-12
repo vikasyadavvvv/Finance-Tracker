98c0f11bc25671cfbdb10ca14fa3181b4cdb41e1
vikasyadavvvv <157904214+vikasyadavvvv@users.noreply.github.com>
2025-08-12 17:31:10 +0530

    Backend deployment

--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,11 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/transactions", transactionRoutes);
 
+app.get('/', (req, res) => {
+  res.send('Hello from Vercel!');
+});
+
+
 const PORT = process.env.PORT || 5000;
 connectDB(process.env.MONGO_URI).then(() => {
   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
